759bd4a8f902ed763dbbc81177e547a7192bc293
Muris Sladić <msladic1@etf.unsa.ba>
2021-05-06 12:18:40 +0200

    Parametri pocinju velikim slovom

--- LifePlanner/Models/RegistrovaniKorisnik.cs
+++ LifePlanner/Models/RegistrovaniKorisnik.cs
@@ -7,81 +7,81 @@ namespace LifePlanner.Models
 {
     public class RegistrovaniKorisnik : IVoda, ITask, IJelo, IRaspolozenje, ITrening
     {
-        public int id { get; set; }
-        public string ime { get; set; }
-        public string prezime { get; set; }
-        public string username { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public List<Task> taskovi { get; set; }
-        public List<Jelo> jela { get; set; }
-        public List<Trening> treninzi { get; set; }
-        public List<Voda> popijenaVoda { get; set; }
-        public List<Raspolozenje> raspolozenja { get; set; }
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Task> Taskovi { get; set; }
+        public List<Jelo> Jela { get; set; }
+        public List<Trening> Treninzi { get; set; }
+        public List<Voda> PopijenaVoda { get; set; }
+        public List<Raspolozenje> Raspolozenja { get; set; }
 
         public void dodajJelo(Jelo jelo)
         {
-            jela.Add(jelo);
+            Jela.Add(jelo);
         }
 
         public void dodajRaspolozenje(Raspolozenje raspolozenje)
         {
-            raspolozenja.Add(raspolozenje);
+            Raspolozenja.Add(raspolozenje);
         }
 
         public void dodajTask(Task task)
         {
-            taskovi.Add(task);
+            Taskovi.Add(task);
         }
 
         public void dodajTrening(Trening trening)
         {
-            treninzi.Add(trening);
+            Treninzi.Add(trening);
         }
 
         public void dodajVodu(Voda voda)
         {
-            popijenaVoda.Add(voda);
+            PopijenaVoda.Add(voda);
         }
 
         public void obrisiJelo(int id)
         {
-            var jeloZaBrisanje = jela.SingleOrDefault<Jelo>(j => j.id == id);
+            var jeloZaBrisanje = Jela.SingleOrDefault<Jelo>(j => j.id == id);
             if (jeloZaBrisanje == null) return;
-            jela.Remove(jeloZaBrisanje);
+            Jela.Remove(jeloZaBrisanje);
         }
 
         public void obrisiTask(int id)
         {
-            var taskZaBrisanje = taskovi.SingleOrDefault<Task>(j => j.id == id);
+            var taskZaBrisanje = Taskovi.SingleOrDefault<Task>(j => j.id == id);
             if (taskZaBrisanje == null) return;
-            taskovi.Remove(taskZaBrisanje);
+            Taskovi.Remove(taskZaBrisanje);
         }
 
         public void obrisiTrening(int id)
         {
-            var treningZaBrisanje = treninzi.SingleOrDefault<Trening>(j => j.id == id);
+            var treningZaBrisanje = Treninzi.SingleOrDefault<Trening>(j => j.id == id);
             if (treningZaBrisanje == null) return;
-            treninzi.Remove(treningZaBrisanje);
+            Treninzi.Remove(treningZaBrisanje);
         }
 
         public void urediJelo(int id, Jelo jelo)
         {
-            var jeloZaPromjenu = jela.SingleOrDefault<Jelo>(j => j.id == id);
+            var jeloZaPromjenu = Jela.SingleOrDefault<Jelo>(j => j.id == id);
             if (jeloZaPromjenu == null) return;
             jeloZaPromjenu = jelo;
         }
 
         public void urediTask(int id, Task task)
         {
-            var taskZaPromjenu = taskovi.SingleOrDefault<Task>(t => t.id == id);
+            var taskZaPromjenu = Taskovi.SingleOrDefault<Task>(t => t.id == id);
             if (taskZaPromjenu == null) return;
             taskZaPromjenu = task;
         }
 
         public void urediTrening(int id, Trening trening)
         {
-            var treningZaPromjenu = treninzi.SingleOrDefault<Trening>(tr => tr.id == id);
+            var treningZaPromjenu = Treninzi.SingleOrDefault<Trening>(tr => tr.id == id);
             if (treningZaPromjenu == null) return;
             treningZaPromjenu = trening;
         }
