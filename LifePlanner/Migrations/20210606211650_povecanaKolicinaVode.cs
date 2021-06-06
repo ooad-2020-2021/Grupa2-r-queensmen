9a4e259f07057ef0b2a4ca1fb78dd54aa807d920
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-06-07 00:25:25 +0200

    povecan decimal za vodu

---
+++ LifePlanner/Migrations/20210606211650_povecanaKolicinaVode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LifePlanner.Migrations
+{
+    public partial class povecanaKolicinaVode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Kolicina",
+                table: "KolicineVode",
+                type: "decimal(7,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Kolicina",
+                table: "KolicineVode",
+                type: "decimal(2,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(7,2)");
+        }
+    }
+}
