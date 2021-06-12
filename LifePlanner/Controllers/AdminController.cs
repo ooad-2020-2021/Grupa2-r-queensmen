b643bca767a1e39109cfd4db516822ef8bbfb6f1
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2021-06-12 12:14:10 +0200

    nebitne izmjene admin kontroleru

---
+++ LifePlanner/Controllers/AdminController.cs
@@ -0,0 +1,119 @@
+﻿using LifePlanner.Data;
+using LifePlanner.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LifePlanner.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<RegistrovaniKorisnik> _userManager;
+
+        public AdminController(ApplicationDbContext context, UserManager<RegistrovaniKorisnik> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            IEnumerable<RegistrovaniKorisnik> korisnici = _context.Users;
+            return View(korisnici);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BrisiKorisnika(string id)
+        {
+            try
+            {
+                RegistrovaniKorisnik korisnik = _context.Users.Where(u => u.Id == id).FirstOrDefault();
+                var result = await _userManager.DeleteAsync(korisnik);
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // GET: AdminController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
